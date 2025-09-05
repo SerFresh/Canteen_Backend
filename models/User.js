7f0b3587ba8f391c22ee8ce03f262c8d55736efe
SerFresh <freshhy75.42@gmail.com>
2025-09-05 20:32:51 +0700

    try again

--- models/User.js
+++ models/User.js
@@ -8,4 +8,4 @@ const UserSchema = new mongoose.Schema({
   imageProfile: { type: String, default: "" },
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema, "users"); 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // server.js
 const express = require("express");
 const connectDB = require("./db"); // ไฟล์เชื่อม MongoDB
+
+const User = require("./models/User");
+
 const app = express();
 
 app.use(express.json());
@@ -12,8 +15,15 @@ app.get("/", (req, res) => {
   res.send("Hello from Vercel API + MongoDB!");
 });
 
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
+// ตัวอย่าง endpoint ดึง user ทั้งหมด
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find(); // ดึงข้อมูลทั้งหมด
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 // Export app แทนการ listen()
