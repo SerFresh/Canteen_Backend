9a4856c912f29ac2d5c0906f763b512aad701a8b
SerFresh <freshhy75.42@gmail.com>
2025-09-05 19:02:04 +0700

    try again

--- server.js
+++ server.js
@@ -52,7 +52,21 @@ app.use(express.json());
 
 connectDB();
 
-app.get("/", (req, res) => res.send("Backend is running!"));
+app.get("/", async (req, res) => {
+  try {
+    if (!mongoose.connections[0].readyState) {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log("MongoDB connected");
+    }
+    res.send("Hello from Vercel API + MongoDB!");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    res.status(500).send("MongoDB connection failed");
+  }
+});
 app.use("/api", authRoutes);
 
 module.exports = app;
