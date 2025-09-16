365a54d5ff5b77197f181b2daf9b92ada89527f8
SerFresh <freshhy75.42@gmail.com>
2025-09-16 10:39:22 +0700

    Canteen 9-16

--- db.js
+++ db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   if (mongoose.connections[0].readyState) return; // reuse existing connection
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err);
