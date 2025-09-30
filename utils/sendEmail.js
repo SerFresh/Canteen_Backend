697511d5415a464c88a19a14403f6b73fe57fa20
SerFresh <freshhy75.42@gmail.com>
2025-09-30 12:15:24 +0700

    fix block render

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, html) {
   try {
     await resend.emails.send({
-      from: "freshhy75.42@gmail.com", 
+      from: "no-reply@yourdomain.com", 
       to,
       subject,
       html,
