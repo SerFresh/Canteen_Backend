ef896c5c0f74877658fad6dacecbc9ba971b1814
SerFresh <freshhy75.42@gmail.com>
2025-10-28 09:38:18 +0700

    email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, html) {
   try {
     await resend.emails.send({
-      from: "canteenkmitl@resend.dev",
+      from: "onboarding@resend.dev",
       to,
       subject,
       html
